f9b7d29b93482d45107fabb1d5becd32f0095ffc
Supratim <supratimnath2002@gmail.com>
2024-05-14 19:13:39 +0530

    minor changes

--- src/components/elements/Banner.tsx
+++ src/components/elements/Banner.tsx
@@ -1,10 +1,77 @@
+import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Image, { StaticImageData } from "next/image";
+import Picture from "../../../public/Group_131.png";
 
-import React from 'react'
+// interface BannerProps {
+//   imgSrc: StaticImageData;
+// }
 
-const Banner: React.FunctionComponent = () => {
+export const Banner: React.FunctionComponent = (props) => {
+  var settings = {
+    dots: false,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: false,
+    autoplaySpeed: 5000,
+    pauseOnHover: true,
+  };
   return (
-    <div>Banner</div>
-  )
-}
+    <div className="flex-auto">
+      <div>
+        <Slider {...settings}>
+          <div>
+            <div>
+              <Image src={Picture} alt="pic1" />
+            </div>
+          </div>
+          <div>
+            <div>
+              <Image src={Picture} alt="pic2" />
+            </div>
+          </div>
+          <div>
+            <div>
+              <Image src={Picture} alt="pic3" />
+            </div>
+          </div>
+          <div>
+            <div>
+              <Image src={Picture} alt="pic4" />
+            </div>
+          </div>
+        </Slider>
+      </div>
 
-export default Banner
+      <div className="flex grid-cols-4">
+        <div className="h-[400px] w-[360px] border border-[#dadada] bg-white">
+          <div className="inset-x-0 bottom-0 break-normal text-[32px] font-semibold text-[#f36562]">
+            Doctor Digital Consult
+          </div>
+        </div>
+        <div className="h-[400px] w-[360px] border border-[#dadada] bg-white">
+          <div className="text-[32px] font-semibold text-[#3b4561]">
+            Clinic / Hospital Visit
+          </div>
+        </div>
+        <div className="h-[400px] w-[360px] border border-[#dadada] bg-white">
+          <div className="break-normal text-[32px] font-semibold text-[#f36562]">
+            Book Pathology Tests
+          </div>
+        </div>
+        <div className="h-[400px] w-[360px] border border-[#dadada] bg-white">
+          <div className="text-[32px] font-semibold text-[#3b4561]">
+            Buy Medicines
+          </div>
+        </div>
+      </div>
+
+      {/* <button className="rounded bg-blue-500 px-4 py-2 font-bold text-white hover:text-blue-400 hover:bg-slate-50">
+        Click me
+      </button> */}
+    </div>
+  );
+};
