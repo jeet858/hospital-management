5cbee2576a50b5addab4007f87a0f7697214b129
Supratim <supratimnath2002@gmail.com>
2024-05-14 21:18:40 +0530

    trial commit

--- src/components/elements/Banner.tsx
+++ src/components/elements/Banner.tsx
@@ -4,6 +4,10 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Image, { StaticImageData } from "next/image";
 import Picture from "../../../public/Group_131.png";
+import Consult from "../../../public/online_medione.png";
+import Patho from "../../../public/pathology_one.png"
+import TreatM from "../../../public/treatment_one.png"
+import Clinic from "../../../public/clinic_one.png"
 
 // interface BannerProps {
 //   imgSrc: StaticImageData;
@@ -47,31 +51,43 @@ export const Banner: React.FunctionComponent = (props) => {
       </div>
 
       <div className="flex grid-cols-4">
-        <div className="h-[400px] w-[360px] border border-[#dadada] bg-white">
+        <div className="flex h-[400px] w-[360px] flex-col justify-end border border-[#dadada] bg-white p-10">
+          <div className="flex justify-end">
+            <Image src={Consult} alt={"doctor digital consult"} />
+          </div>
           <div className="inset-x-0 bottom-0 break-normal text-[32px] font-semibold text-[#f36562]">
-            Doctor Digital Consult
+            Doctor <br />
+            Digital Consult
           </div>
         </div>
-        <div className="h-[400px] w-[360px] border border-[#dadada] bg-white">
+        <div className="flex h-[400px] w-[360px] flex-col justify-end border border-[#dadada] bg-white p-10">
+          <div className="flex justify-end">
+            <Image src={Clinic} alt={"doctor digital consult"} />
+          </div>
           <div className="text-[32px] font-semibold text-[#3b4561]">
-            Clinic / Hospital Visit
+            Clinic /<br /> Hospital Visit
           </div>
         </div>
-        <div className="h-[400px] w-[360px] border border-[#dadada] bg-white">
+        <div className="flex h-[400px] w-[360px] flex-col justify-end border border-[#dadada] bg-white p-10">
+          <div className="flex justify-end">
+            <Image src={Patho} alt={"doctor digital consult"} />
+          </div>
           <div className="break-normal text-[32px] font-semibold text-[#f36562]">
-            Book Pathology Tests
+            Book <br />
+            Pathology Tests
           </div>
         </div>
-        <div className="h-[400px] w-[360px] border border-[#dadada] bg-white">
+        <div className="flex h-[400px] w-[360px] flex-col justify-end border border-[#dadada] bg-white p-10">
+          <div className="flex justify-end">
+            <Image src={TreatM} alt={"doctor digital consult"} />
+          </div>
           <div className="text-[32px] font-semibold text-[#3b4561]">
-            Buy Medicines
+            Buy <br />
+            Medicines
           </div>
         </div>
       </div>
 
-      {/* <button className="rounded bg-blue-500 px-4 py-2 font-bold text-white hover:text-blue-400 hover:bg-slate-50">
-        Click me
-      </button> */}
     </div>
   );
 };
